c49c6a80d96ce5707916838ced8099b9b47aaf9a
DaeV69 <70040510+DaeV69@users.noreply.github.com>
2021-11-25 23:57:41 +0300

    fixed drop down menu onclickoutside bugs

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+
+set -e
+
+# build
+
+npm run build
+
+# navigate into the build output directory
+
+cd dist
+
+git init
+
+git add -A
+
+git commit -m 'new deployemnt'
+
+git push -f git@github.com:DaeV69/moefetch-client.git master:gh-pages
+
+cd -
