2bb57c10f7ed5a435a6a414e3d0688908f095898
DaeV69 <70040510+DaeV69@users.noreply.github.com>
2021-11-26 22:27:20 +0300

    new deployemnt

---
+++ assets/Home.405baf75.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.efedc7a0.js";import{o as r,c}from"./vendor.a98cd8a7.js";const o={};function a(n,s){return r(),c("h2",null,"sexx")}var f=e(o,[["render",a]]);export{f as default};
---
+++ assets/Page1.92573ff6.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.efedc7a0.js";import{o as r,c as a}from"./vendor.a98cd8a7.js";const c={};function o(n,t){return r(),a("h2",null,"page1")}var f=e(c,[["render",o]]);export{f as default};
