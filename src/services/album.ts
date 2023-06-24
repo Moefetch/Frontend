d28830351d8426c2352207f7028078934a6ed367
DaeV69 <70040510+DaeV69@users.noreply.github.com>
2023-06-24 17:18:26 +0300

    last changes idk

--- src/services/album.ts
+++ src/services/album.ts
@@ -3,7 +3,7 @@ import { Picture } from "./picture";
 import { IAlbum, IPicture } from "./types";
 import { SelectableItem } from "./selectableItem";
 
-import { state } from '../../state'
+import { state } from "../../state";
 
 export class Album extends SelectableItem implements IAlbum {
   public albumCoverImage: string;
@@ -12,61 +12,67 @@ export class Album extends SelectableItem implements IAlbum {
   public type: string;
   public estimatedPicCount: number;
 
-  public pictures: Picture[] = []
-  
+  public pictures: Picture[] = [];
+
   public constructor(properties: IAlbum) {
-      super();
-      this.albumCoverImage = properties.albumCoverImage;            
-      this.name = properties.name;
-      this.uuid = properties.uuid;
-      this.type = properties.type;
-      this.estimatedPicCount = properties.estimatedPicCount;            
+    super();
+    this.albumCoverImage = properties.albumCoverImage;
+    this.name = properties.name;
+    this.uuid = properties.uuid;
+    this.type = properties.type;
+    this.estimatedPicCount = properties.estimatedPicCount;
   }
 
-  public getPictures(options?: {sortBy?: string, tags?: string[], nameIncludes?: string}) {
-    api.getPicsInAlbum(
-      this.uuid,
-      {
+  public getPictures(options?: {
+    sortBy?: string;
+    tags?: string[];
+    nameIncludes?: string;
+  }) {
+    api
+      .getPicsInAlbum(this.uuid, {
         showNSFW: state.stateVariables.showNSFW,
         showHidden: state.stateVariables.showHidden,
-        sortBy: options?.sortBy ?? 'date_added',
+        sortBy: options?.sortBy ?? "date_added",
         tags: options?.tags,
         nameIncludes: options?.nameIncludes,
-      }
-    ).then((pictures) => {
-      this.pictures = pictures.map((pic)=> new Picture(pic))
-    }).catch(console.log)
+      })
+      .then((pictures) => {
+        this.pictures = pictures.map((pic) => new Picture(pic));
+      })
+      .catch(console.log);
   }
 
   /**
-  * deleteSelectedPics
-  */
+   * deleteSelectedPics
+   */
   public deleteSelectedPics() {
-    api.deletePicturesInAlbum(this.name, 
-      this.pictures.filter( (p, index) => p.isSelected && this.pictures.splice(index, 1)).map(p => p.id)
-    )
-    
-    state.stateVariables.isEditing = false;
-  }
-/**
- * addPictures
- */
-public addPictures(pictures: IPicture[]) {
-  pictures.forEach(pic => {
-    const pictureInstance = new Picture(pic)
-    this.pictures.push(pictureInstance);
-  })
-}
-  /**
-  * deleteSelectedPics
-  */
-  public handleHiding(hide: boolean) {
-    api.handleHidingPicturesInAlbum(this.name, 
-      this.pictures.filter(p => p.isSelected).map(p => p.id),
-      hide
-    )
-    state.stateVariables.isEditing = false;
-  }
+    api.deletePicturesInAlbum(
+      this.name,
+      this.pictures
+        .filter((p, index) => p.isSelected && this.pictures.splice(index, 1))
+        .map((p) => p.id)
+    );
 
-  
+    state.stateVariables.isEditing = false;
+  }
+  /**
+   * addPictures
+   */
+  public addPictures(pictures: IPicture[]) {
+    pictures.forEach((pic) => {
+      const pictureInstance = new Picture(pic);
+      this.pictures.push(pictureInstance);
+    });
+  }
+  /**
+   * deleteSelectedPics
+   */
+  public handleHiding(hide: boolean) {
+    api.handleHidingPicturesInAlbum(
+      this.name,
+      this.pictures.filter((p) => p.isSelected).map((p) => p.id),
+      hide
+    );
+    state.stateVariables.isEditing = false;
+  }
 }
--- src/views/Album.vue
+++ src/views/Album.vue
@@ -1,25 +1,35 @@
 <template>
-  <div class="pictures_container grid" v-if="album.pictures"
+  <div
+    class="pictures_container grid"
+    v-if="album.pictures"
     @mouseup="mouseRelease()"
-    @mousedown="mouseClickBackground()">
-    
-    <PictureItem v-for="picture, index in album.pictures" :picture="picture" :key="picture.id"
+    @mousedown="mouseClickBackground()"
+  >
+    <PictureItem
+      v-for="(picture, index) in album.pictures"
+      :picture="picture"
+      :key="picture.id"
       @click="handleMouseClick(picture, index)"
-      @mousedown="holdDownMouseStart()" 
+      @mousedown="holdDownMouseStart()"
       :selected="picture.isSelected && state.stateVariables.isEditing"
       @mouseup="mouseDownBool = false"
       @mouseover="hoverOverPics(picture)"
-      @mouseleave="handleMouseLeave(picture)" />
-    
+      @mouseleave="handleMouseLeave(picture)"
+    />
+
     <PopupSlot v-if="state.stateVariables.popup == 'ViewingPicture'">
-      <ViewingPicture :item="album.pictures[picIndexer]" :backendUrl='backendUrl'/>
+      <ViewingPicture
+        :item="album.pictures[picIndexer]"
+        :backendUrl="backendUrl"
+      />
     </PopupSlot>
-    
-    <EditControls 
-    :deleteSelection="() => album.deleteSelectedPics()" 
-    :hideSelection="() => album.handleHiding(true)" 
-    :unhideSelection="() => album.handleHiding(false)" 
-    v-if="state.stateVariables.isEditing" />
+
+    <EditControls
+      :deleteSelection="() => album.deleteSelectedPics()"
+      :hideSelection="() => album.handleHiding(true)"
+      :unhideSelection="() => album.handleHiding(false)"
+      v-if="state.stateVariables.isEditing"
+    />
   </div>
 </template>
 
@@ -31,107 +41,110 @@ import { ref, onMounted, inject, watch, computed } from "vue";
 import PictureItem from "../components/Cards/PictureItem.vue";
 import EditControls from "../components/Misc/EditControls.vue";
 
-import ViewingPicture from '../components/Popups/ViewingPicture.vue'
+import ViewingPicture from "../components/Popups/ViewingPicture.vue";
 import PopupSlot from "../components/Misc/PopupSlot.vue";
 
-import { AppState } from '../../state'
+import { AppState } from "../../state";
 import { Picture } from "../services/picture";
-import { onKeyStroke } from '@vueuse/core'
+import { onKeyStroke } from "@vueuse/core";
 
-const backendUrl = api.getBackendUrl()
+const backendUrl = api.getBackendUrl();
 const route = useRoute();
-const state = (inject('state') as AppState)
+const state = inject("state") as AppState;
 
-const album = computed(()=>{
-  return state.stateVariables.albums[route.params.albumUUID as string]
-})
+const album = computed(() => {
+  return state.stateVariables.albums[route.params.albumUUID as string];
+});
 
-const picIndexer = ref(0)
+const picIndexer = ref(0);
 const mouseDownBool = ref(false);
 const mouseDownOnPic = ref(false);
 
 function mouseClickBackground() {
-  if(state.stateVariables.popup) turnOffMouseDown()
+  if (state.stateVariables.popup) turnOffMouseDown();
   else mouseDownBool.value = true;
 }
 
-onKeyStroke('ArrowLeft', (e) => {
-  if (picIndexer.value != 0) { 
-    picIndexer.value = --picIndexer.value
+onKeyStroke("ArrowLeft", (e) => {
+  if (state.stateVariables.popup != "AddNewPicturePopup") {
+    if (picIndexer.value != 0) {
+      picIndexer.value = --picIndexer.value;
+    }
+    e.preventDefault();
   }
-  e.preventDefault()
-})
+});
 
-onKeyStroke('ArrowRight', (e) => {
-  if (picIndexer.value != (album.value.pictures.length - 1)) {
-    picIndexer.value = ++picIndexer.value;
+onKeyStroke("ArrowRight", (e) => {
+  if (state.stateVariables.popup != "AddNewPicturePopup") {
+    if (picIndexer.value != album.value.pictures.length - 1) {
+      picIndexer.value = ++picIndexer.value;
+    }
+    e.preventDefault();
   }
-  e.preventDefault()
-})
+});
 
 onMounted(async () => {
   album.value.getPictures(state.stateVariables.advancedSearchOptions);
-  state.clearAdvancedSearchOptions()
+  state.clearAdvancedSearchOptions();
 });
 
 function toggleSelection(picture: Picture) {
-  if (picture.isSelected) picture.deselect()
-  else picture.select()
+  if (picture.isSelected) picture.deselect();
+  else picture.select();
 }
 
 function turnOffMouseDown() {
-  mouseDownBool.value = false; 
+  mouseDownBool.value = false;
   mouseDownOnPic.value = false;
 }
 function handleMouseClick(picture: Picture, index: number) {
   if (!state.stateVariables.isEditing) {
-    turnOffMouseDown()
+    turnOffMouseDown();
     picIndexer.value = index;
-    state.stateVariables.popup = 'ViewingPicture';
+    state.stateVariables.popup = "ViewingPicture";
   } else {
-    toggleSelection(picture)
+    toggleSelection(picture);
   }
-
 }
 
 function holdDownMouseStart() {
- /*  if (state.stateVariables.isEditing) {
+  /*  if (state.stateVariables.isEditing) {
     picture.select()
     
   }   */
-  mouseDownOnPic.value = true
-  mouseDownBool.value = true
+  mouseDownOnPic.value = true;
+  mouseDownBool.value = true;
 }
 
 function hoverOverPics(picture: Picture) {
   if (mouseDownBool.value && state.stateVariables.isEditing) {
-    toggleSelection(picture)
+    toggleSelection(picture);
   }
-  
-} 
+}
 //selects first image when not editing then enables editing
 function handleMouseLeave(picture: Picture) {
-  
   if (!state.stateVariables.isEditing && mouseDownBool.value) {
-
     state.stateVariables.isEditing = true;
     //toggleSelection(picture)
-  } 
+  }
   if (state.stateVariables.isEditing && mouseDownOnPic.value) {
     mouseDownOnPic.value = false;
-    toggleSelection(picture)
+    toggleSelection(picture);
   }
 }
 
-function mouseRelease(){
-  turnOffMouseDown()
+function mouseRelease() {
+  turnOffMouseDown();
 }
-watch(()=> state.stateVariables.isEditing, ()=> {
-  if (!state.stateVariables.isEditing) {
-    album.value.pictures.forEach(pic => pic.deselect())
-    turnOffMouseDown();
+watch(
+  () => state.stateVariables.isEditing,
+  () => {
+    if (!state.stateVariables.isEditing) {
+      album.value.pictures.forEach((pic) => pic.deselect());
+      turnOffMouseDown();
+    }
   }
-})
+);
 </script>
 
 <style lang="postcss">
